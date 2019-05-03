b704c3dc47b884d16494f0eed9404752ea158109
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-05-03 23:49:01 +0300

    Implement exercise 1.5

--- kierros-1/kurssitiedot/src/index.js
+++ kierros-1/kurssitiedot/src/index.js
@@ -1,44 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => (<h1>{props.course}</h1>)
+const Header = (props) => (<h1>{props.course.name}</h1>)
 const Part = (props) => {
     console.log(props)
     return <p>{props.part.name} {props.part.exercises}</p>
 }
 const Content = (props) => 
 {
-  const partProps = props.props
-  console.log(partProps)
+  const parts = props.course.parts
+  console.log(parts)
   return <div>
-        <Part part={partProps[0]} />
-        <Part part={partProps[1]} />
-        <Part part={partProps[2]} />
+        <Part part={parts[0]} />
+        <Part part={parts[1]} />
+        <Part part={parts[2]} />
     </div>
 }
-const Total = (parts) => (<p>yhteensä {parts.parts.reduce((acc,val,)=>acc+val.exercises,0)} tehtävää</p>)
+const Total = (props) => (<p>yhteensä {props.course.parts.reduce((acc,val,)=>acc+val.exercises,0)} tehtävää</p>)
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [{
+            name: 'Reactin perusteet',
+            exercises: 10
+          },
+          {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7
+          },
+          {
+            name: 'Komponenttien tila',
+            exercises: 14
+          }]
+    }
 
-    const parts = [{
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
-      }]
-  
     return (
       <div>
         <Header course={course} />
-        <Content props={parts} />
-        <Total parts={parts} />
+        <Content course={course} />
+        <Total course={course} />
       </div>
     )
   }
