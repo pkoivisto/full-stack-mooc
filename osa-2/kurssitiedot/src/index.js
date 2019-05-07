888d086056ddb223afa0df5a3823b4e0ced90188
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-05-07 21:18:34 +0300

    Support multiple courses for kurssitiedot app (exercise 2.4)

--- osa-2/kurssitiedot/src/index.js
+++ osa-2/kurssitiedot/src/index.js
@@ -20,28 +20,53 @@ const Course = ({course}) =>
   </div>
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [{
-            name: 'Reactin perusteet',
-            exercises: 10
-          },
-          {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7
-          },
-          {
-            name: 'Komponenttien tila',
-            exercises: 14
-          }]
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
     }
+  ]
 
     return (
-      <div>
-        <Course course={course} />
-        <Total course={course} />
+      courses.map(course => 
+        <div key={course.id}>
+          <Course course={course} />
+          <Total course={course} />
         </div>
-    )
+      ))
   }
 
 ReactDOM.render(<App />, document.getElementById('root'))
