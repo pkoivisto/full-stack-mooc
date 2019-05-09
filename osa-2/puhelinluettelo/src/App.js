df14236767da18f7bf7d348d5cdafce31a4e26a9
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-05-09 22:07:49 +0300

    Add input field to filter phone book entries (exercise 2.9)

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -2,23 +2,40 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '045-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  const addPerson = (ev) => {
+    ev.preventDefault()
+    if(persons.map(p => p.name).includes(newName)) {
+      alert(`${newName} on jo luettelossa`)
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}))
+    }
+  }
+
+  const byNamefilter = (person) => person.name.toLowerCase().includes(filter.toLowerCase())
+
+  const filteredList = persons.filter(byNamefilter)
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
-      <form>
-        <div>
-          nimi: <input />
-        </div>
+      Rajaa näytettäviä: <input value={filter} onChange={(event) => setFilter(event.target.value)}/>
+
+      <h3>Lisää uusi</h3>
+      <form onSubmit={addPerson}>
+        <div>nimi: <input value={newName} onChange={(event) => setNewName(event.target.value)}/></div>
+        <div>numero: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/></div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      ...
+      <div>{filteredList.map(person => <p key={person.name}>{person.name} {person.number}</p>)}</div>
     </div>
   )
 
