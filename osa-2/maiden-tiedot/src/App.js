3df1d00f996cbd3326172630eefb19cae8b1521c
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-05-16 21:33:30 +0300

    Implement first version of maiden-tiedot app (exercise 2.12)

--- osa-2/maiden-tiedot/src/App.js
+++ osa-2/maiden-tiedot/src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect }  from 'react';
+import axios from 'axios';
 
-function App() {
+const FilteredCountries = ({countries, filter}) => {
+  const filtered = countries.filter(c => c.toLowerCase().includes(filter.toLowerCase()))
+  if (filtered.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  } else {
+    return filtered.map(c => <p key={c}>{c}</p>)
+  }
+}
+
+const App = () => {
+
+  const [ countries, setCountries ] = useState([])
+  const [ countryFilter, setCountryFilter ] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(response => setCountries(response.data.map(o => o.name)))},
+    [])
+
   return (
-    <div className="App">
-      empty canvas here...
+    <div>
+      <div>find countries <input value={countryFilter} onChange={(e) => setCountryFilter(e.target.value)}/></div>
+      <FilteredCountries countries={countries} filter={countryFilter}/>
     </div>
   );
 }
