43cfb5ee07513ee0dc6939bf06a4dbc2a3d3f40c
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-05-20 21:59:24 +0300

    Exercises 2.19 & 2.20: Add a notification component along with CSS-styles to display whether modifying data was successful or not

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PersonsList from './components/personsList.js'
 import PersonForm from './components/personForm.js'
 import Filter from './components/filter.js'
+import Notification from './components/notification.js'
 
 import { getAllPersons, addPerson, deletePerson, updatePerson } from './personService.js'
 
@@ -10,21 +11,29 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [ notification, setNotification ] = useState({})
 
   useEffect(() => {
     getAllPersons().then(persons => setPersons(persons))
   }, [])
+
+  const notifyWithTimeout = (message, success) => {
+    setNotification({message, success})
+    setTimeout(() => {
+      setNotification({})
+    }, 3000)
+  }
 
   const doAddPerson = (ev) => {
     ev.preventDefault()
     if(persons.map(p => p.name).includes(newName)) {
       if (window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella?`)) {
         const existing = persons.find(p => p.name.includes(newName))
-        updatePerson({...existing, number: newNumber}).then(data => setPersons(persons.filter(p => p.id !== data.id).concat(data)))
+        updatePerson({...existing, number: newNumber}).then(data => setPersons(persons.filter(p => p.id !== data.id).concat(data))).then(() => notifyWithTimeout(`Päivitettiin henkilön ${newName} puhelinnumero`, true))
       }
     } else {
       const newPerson = {name: newName, number: newNumber}
-      addPerson(newPerson).then(data => setPersons(persons.concat(data)))
+      addPerson(newPerson).then(data => setPersons(persons.concat(data))).then(() => notifyWithTimeout(`Lisättiin ${newName} puhelinluetteloon`, true))
     }
   }
 
@@ -34,13 +43,19 @@ const App = () => {
 
   const deleteWithConfirm = ({id, name}) => { 
     if (window.confirm(`Haluatko todella poistaa henkilön ${name}?`)) { 
-        deletePerson(id).then(success => setPersons(persons.filter(p => p.id !== id)))
+        deletePerson(id)
+          .then(() => notifyWithTimeout(`Poistettiin ${name} puhelinluettelosta`, true))
+          .catch(() => notifyWithTimeout(`Henkilö ${name} oli jo poistettu`, false))
+        // It is assumed that in case of an error the person was already removed (in another session);
+        // thus, we filter out the person regardless of response code.
+        setPersons(persons.filter(p => p.id !== id))
       }
     }
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <Notification notification={notification} />
       <Filter filter={filter} setFilter={setFilter} />
       <h3>Lisää uusi</h3>
       <PersonForm addPerson={doAddPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
