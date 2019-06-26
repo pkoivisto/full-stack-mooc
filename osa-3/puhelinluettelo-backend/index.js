920596848bfec71d32a396cd0c9860b63a1210f4
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-06-26 22:01:02 +0300

    Implement features for exercises 3.3 and 3.4

--- osa-3/puhelinluettelo-backend/index.js
+++ osa-3/puhelinluettelo-backend/index.js
@@ -18,6 +18,31 @@ app.get('/api/persons', (req, res) => {res.json(persons)})
 
 app.get('/info', (req, res) => {res.send(`<div><p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p></div>`)})
 
+const personForRequest = req => {
+    const id = Number(req.params.id);
+    return persons.find(elem => elem.id === id);
+} 
+
+app.get('/api/persons/:id', (req, res) => 
+    {
+        const person = personForRequest(req);
+        if (person) {
+            res.json(person);
+        } else {
+            res.status(404).end();
+        }
+    })
+
+app.delete('/api/persons/:id', (req, res) => {
+    const idx = persons.findIndex(elem => elem.id === Number(req.params.id))
+    if (idx === -1) {
+        res.status(404).end();
+    } else {
+        persons.splice(idx, 1);
+        res.status(200).end();
+    }
+})
+
 const PORT=3001
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
