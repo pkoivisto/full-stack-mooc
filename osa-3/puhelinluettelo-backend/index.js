92c8e9ccf78632268a6dee6874985b23297f7aad
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-02 22:31:36 +0300

    Implement POSTing a new person (ex. 3.5), add validation also (ex 3.6)

--- osa-3/puhelinluettelo-backend/index.js
+++ osa-3/puhelinluettelo-backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -19,27 +22,42 @@ app.get('/api/persons', (req, res) => {res.json(persons)})
 app.get('/info', (req, res) => {res.send(`<div><p>Phonebook has info for ${persons.length} people.</p><p>${new Date()}</p></div>`)})
 
 const personForRequest = req => {
-    const id = Number(req.params.id);
-    return persons.find(elem => elem.id === id);
+    const id = Number(req.params.id)
+    return persons.find(elem => elem.id === id)
 } 
 
 app.get('/api/persons/:id', (req, res) => 
     {
-        const person = personForRequest(req);
+        const person = personForRequest(req)
         if (person) {
-            res.json(person);
+            res.json(person)
         } else {
-            res.status(404).end();
+            res.status(404).end()
         }
     })
 
 app.delete('/api/persons/:id', (req, res) => {
     const idx = persons.findIndex(elem => elem.id === Number(req.params.id))
     if (idx === -1) {
-        res.status(404).end();
+        res.status(404).end()
     } else {
-        persons.splice(idx, 1);
-        res.status(200).end();
+        persons.splice(idx, 1)
+        res.status(200).end()
+    }
+})
+
+const MAX_ID = 1000000;
+app.post('/api/persons/', (req, res) => {
+    const { name, number } = req.body
+    if ( !name || !number) {
+        res.status(400).json({"error" : "name or number undefined"})
+    } else if (persons.find(p => p.name === name) !== undefined) {
+        res.status(400).json({"error" : "name must be unique"})
+    } else {
+        const id = Math.floor(Math.random() * MAX_ID)
+        const person = {...req.body, id: id}
+        persons.push(person)
+        res.status(200).json(person)
     }
 })
 
