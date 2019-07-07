77533f3cfbb61b316acff1da6e71d9e8a6e0e37c
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-07 16:34:35 +0300

    Exercise 3.10: Fix environment variable for heroku app

--- osa-3/puhelinluettelo-backend/index.js
+++ osa-3/puhelinluettelo-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-var morgan = require('morgan')
+const morgan = require('morgan')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -83,7 +83,7 @@ app.post('/api/persons/', (req, res) => {
     }
 })
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
