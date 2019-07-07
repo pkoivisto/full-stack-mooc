7a8af4dddfcbd816ae97a5fcfb669cadf6b165e4
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-07 16:19:51 +0300

    Exercise 3.9: use domain relative URLs for resources instead of static URLs; set up proxy.

--- osa-3/puhelinluettelo-frontend/src/personService.js
+++ osa-3/puhelinluettelo-frontend/src/personService.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const getAllPersons = () => axios.get('http://localhost:3001/api/persons').then(response => response.data)
+const getAllPersons = () => axios.get('/api/persons').then(response => response.data)
 
-const addPerson = (data) => axios.post('http://localhost:3001/api/persons', data).then(response => response.data)
+const addPerson = (data) => axios.post('/api/persons', data).then(response => response.data)
 
-const deletePerson = (id) => axios.delete(`http://localhost:3001/api/persons/${id}`).then(response => response.data)
+const deletePerson = (id) => axios.delete(`/api/persons/${id}`).then(response => response.data)
 
-const updatePerson = (data) => axios.put(`http://localhost:3001/api/persons/${data.id}`, data).then(response => response.data)
+const updatePerson = (data) => axios.put(`/api/persons/${data.id}`, data).then(response => response.data)
 
 export { getAllPersons, addPerson, deletePerson, updatePerson }
