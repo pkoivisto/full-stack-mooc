a3d0b7a2fb01b20f209b576a860f64fcd50e4c1b
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-09 21:36:32 +0300

    Exercise 3.12: mongo-backend using mongoose to connect to MongoAtlas

---
+++ osa-3/puhelinluettelo-backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const dbUrl = `mongodb+srv://fullstack:${password}@testiklusteri-a8szo.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(dbUrl, {useNewUrlParser: true})
+const personSchema = new mongoose.Schema({name: String, number: String, id: Number})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    console.log("phonebook:")
+    Person.find({}).then(persons => {
+        persons.forEach(p => console.log(p.name, p.number))
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log('confused, exiting..')
+    process.exit(1)
+}
