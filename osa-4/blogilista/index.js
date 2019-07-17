0c3d5f6767be308b9220c6a47f7ae33650ae6688
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-17 22:34:27 +0300

    Ex 4.2: refactor app, part 1 - extract config and model to their own files

--- osa-4/blogilista/index.js
+++ osa-4/blogilista/index.js
@@ -1,23 +1,13 @@
-require('dotenv').config()
-
 const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
+const { MONGODB_URI, PORT } = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -40,7 +30,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
