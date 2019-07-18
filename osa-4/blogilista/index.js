ba6409490651eada7ab3d7746ac5b0039212f38f
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-18 22:17:50 +0300

    Exercise 4.2: extract api routes handling to controllers/blogs.js

--- osa-4/blogilista/index.js
+++ osa-4/blogilista/index.js
@@ -1,34 +1,16 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 const { MONGODB_URI, PORT } = require('./utils/config')
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
