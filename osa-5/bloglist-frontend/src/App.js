2d6e4533dddab53cdb70372bca5d095d6f0a177d
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-27 19:36:34 +0300

    Exercises 5.1-5.2: implement frontend with login and logout functionality, store logged in localStorage

--- osa-5/bloglist-frontend/src/App.js
+++ osa-5/bloglist-frontend/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import blogService from './services/blogs'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import localStorage from './services/localStorage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const storedUser = localStorage.getStoredUser()
+    if (storedUser) {
+     setUser(storedUser)
+    }
+  }, [])
+
+  useEffect(() => {
+    async function fetchBlogs() {
+      if (user) {
+        const blogs = await blogService.getAll()
+        setBlogs(blogs)
+      }
+    }
+    fetchBlogs()
+  }, [user])
+
+  const handleLogin = (user) => {
+    localStorage.setStoredUser(user)
+    setUser(user)
+  }
+
+  if (!user) {
+    return (
+      <LoginForm setUser={handleLogin}/>
+    );
+  } else {
+    return (
+      <div>
+      <h2>Blogs</h2>
+      <p>
+        {user.name} is logged in <button onClick={() => { localStorage.removeStoredUser(); setUser(null) }}>log out</button>
+      </p>
+      { blogs.map(blog => <Blog key={blog.id} blog={blog}/>) }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ osa-5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import loginService from '../services/login'
+
+const LoginForm = ({ setUser }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+  
+    const doLogin = async (e) => {
+      e.preventDefault()
+      try {
+        const user = await loginService.login({username, password})
+        setUser(user)
+      } catch (exception) {
+        console.log(exception)
+      }
+    }
+    
+    return <form onSubmit={doLogin}> 
+      <h2>Login to application</h2>
+      <div>
+        Username
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+      </div>
+      <div>
+        Password
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  }
+
+  export default LoginForm
