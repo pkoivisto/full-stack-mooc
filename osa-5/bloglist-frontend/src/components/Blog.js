defddaa3b50a48f547f00b08ed7932bb05f159cb
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-07-28 15:34:16 +0300

    Exercise 5.6: display additional details when clicking on blog name row

--- osa-5/bloglist-frontend/src/components/Blog.js
+++ osa-5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,21 @@
-import React from 'react'
-const Blog = ({ blog, style }) => (
-  <div style={style}>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog, style}) => {
+  const [fullDetailsVisible, setFullDetailsVisible] = useState(false)
+  const toggleFullDetails = () => setFullDetailsVisible(!fullDetailsVisible)
+  const showWhenFullDetailsVisible = { display : ( fullDetailsVisible ? '' : 'none' ) }
+
+  return (
+    <div style={style}>
+      <div onClick={toggleFullDetails}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenFullDetailsVisible}>
+        <a href={blog.url}>{blog.url}</a>
+        <div>{blog.likes} likes <button onClick={() => console.log('clicked!')}>like</button></div>
+        <div>added by {blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
