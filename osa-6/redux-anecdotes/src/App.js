0b93c9471c9687e62a402a765ccaf51e36ffa41a
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-08-04 14:14:51 +0300

    Exercise 6.16: initialize app state with anecdotes loaded from json-server backend

--- osa-6/redux-anecdotes/src/App.js
+++ osa-6/redux-anecdotes/src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import { initAnecdotes } from './reducers/anecdoteReducer'
 
-const App = () => {
+const App = ({ initAnecdotes }) => {
+  useEffect(() => { anecdoteService.getAll().then(data => initAnecdotes(data)) }, [initAnecdotes])
 
   return (
     <div>
@@ -17,4 +21,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(null, { initAnecdotes })(App)
--- osa-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,9 +8,7 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE_ANECDOTE':
       const anecdoteIndex = state.findIndex(val => val.id === action.id)
@@ -28,6 +17,10 @@ const anecdoteReducer = (state = initialState, action) => {
       return newState
     case 'CREATE_ANECDOTE':
       return [...state, asObject(action.content)]
+    case 'INIT_ANECDOTES':
+      const initState = action.contents
+      console.log("INIT_ANECDOTES, state will be: ", initState)
+      return initState
     default:
       return state
   }
@@ -38,7 +31,11 @@ export const vote = (id) => {
 }
 
 export const createAnecdote = (content) => {
-  return {type: 'CREATE_ANECDOTE', content }
+  return { type: 'CREATE_ANECDOTE', content }
+}
+
+export const initAnecdotes = (contents) => {
+  return { type: 'INIT_ANECDOTES', contents }
 }
 
 export default anecdoteReducer
