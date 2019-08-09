08e2b72bc3722f01ee998fd4c433efc57337c021
Pyry Koivisto <pyry.koivisto@iki.fi>
2019-08-09 22:09:48 +0300

    Exercise 7.7: show basic details for all users under path /users

--- osa-7/bloglist-frontend/src/App.js
+++ osa-7/bloglist-frontend/src/App.js
@@ -4,10 +4,11 @@ import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
+import Users from './components/Users'
 import Notification from './components/Notification'
 import localStorage from './services/localStorage'
-
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const App = ({ notification }) => {
   const [user, setUser] = useState(null)
@@ -47,18 +48,27 @@ const App = ({ notification }) => {
     )
   } else {
     return (
-      <div>
-        <h2>Blogs</h2>
-        <Notification {...notification} />
+      <Router>
         <div>
-          {user.name} is logged in <button onClick={() => { localStorage.removeStoredUser(); setUser(null) }}>log out</button>
+          <div>
+            <Link to="/users">Users</Link>
+          </div>
+          <h2>Blogs</h2>
+          <Notification {...notification} />
+          <div>
+            {user.name} is logged in <button onClick={() => { localStorage.removeStoredUser(); setUser(null) }}>log out</button>
+          </div>
+          <p/>
+          <Route path="/users" render={() => <Users />}/>
+          <Route exact path="/" render={() => (
+            <div><Togglable label="New blog entry">
+              <BlogForm user={user}/>
+            </Togglable>
+            { blogs.map((blog, idx) => <Blog key={blog.id} blog={blog} style={idx % 2 === 0 ? evenRowStyle : oddRowStyle} loggedInUser={user.username}/>) }
+            </div>)
+          }/>
         </div>
-        <p/>
-        <Togglable label="New blog entry">
-          <BlogForm user={user}/>
-        </Togglable>
-        { blogs.map((blog, idx) => <Blog key={blog.id} blog={blog} style={idx % 2 === 0 ? evenRowStyle : oddRowStyle} loggedInUser={user.username}/>) }
-      </div>
+      </Router>
     )
   }
 }
---
+++ osa-7/bloglist-frontend/src/components/Users.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import userService from '../services/users'
+
+const Users = () => {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    const getAll = async () => {
+      const response = await userService.getAll()
+      setUsers(response)
+    }
+    getAll()
+  }, [])
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => <tr key={user.id}><td>{user.name}</td><td>{user.blogs.length}</td></tr>)}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Users
